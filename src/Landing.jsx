b4e4bf20dcc6a9449d1870d9837d736336d7579b
Udara Neminda Chandrawansa <udaraneminda@gmail.com>
2024-12-06 18:47:12 +0530

    carousel ind fixed

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -276,21 +276,21 @@ function Landing() {
           <div className="absolute z-30 flex space-x-3 -translate-x-1/2 rtl:space-x-reverse bottom-5 left-1/2">
             <button
               type="button"
-              className="w-3 h-3 rounded-full"
+              className="w-[50px] h-1 rounded-full"
               aria-current="true"
               aria-label="Slide 1"
               data-carousel-slide-to="0"
             ></button>
             <button
               type="button"
-              className="w-3 h-3 rounded-full"
+              className="w-[50px] h-1 rounded-full"
               aria-current="false"
               aria-label="Slide 2"
               data-carousel-slide-to="1"
             ></button>
             <button
               type="button"
-              className="w-3 h-3 rounded-full"
+              className="w-[50px] h-1 rounded-full"
               aria-current="false"
               aria-label="Slide 3"
               data-carousel-slide-to="2"
