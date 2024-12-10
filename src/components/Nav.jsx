2fcc3718c0f3549f0043419283d96cd67fe0b70b
Udara Neminda Chandrawansa <udaraneminda@gmail.com>
2024-12-10 23:26:05 +0530

    new nav modal added

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -59,82 +59,98 @@ function Nav() {
       {/*burger button*/}
       <div
         className="hidden place-items-center h-[10dvh] px-10 cursor-pointer place-content-center py-6 max-[940px]:grid text-black"
-        data-modal-target="nav-modal"
-        data-modal-toggle="nav-modal"
-        onClick={()=>alert("Test")}
+        data-coreui-toggle="modal"
+        data-coreui-target="#staticBackdrop"
       >
         <img src={burger} alt="burger-icon" />
       </div>
       {/*Nav Modal for small screens*/}
       <div
-        id="nav-modal"
-        data-modal-backdrop="static"
+        className="modal fade"
+        id="staticBackdrop"
+        data-coreui-backdrop="static"
+        data-coreui-keyboard="false"
         tabIndex="-1"
+        aria-labelledby="staticBackdropLabel"
         aria-hidden="true"
-        className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
       >
-        <div className="relative w-full max-w-2xl max-h-full p-4">
-          <div className="relative bg-white rounded-lg shadow">
-            {/*head*/}
-            <div className="flex items-center justify-between p-2 border-b rounded-t md:p-5">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
               <button
                 type="button"
-                className="inline-flex items-center justify-center w-8 h-8 text-sm text-gray-400 bg-transparent rounded-lg hover:bg-gray-200 hover:text-gray-900 ms-auto"
-                data-modal-hide="nav-modal"
-              >
-                <svg
-                  className="w-3 h-3"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 14 14"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-                  />
-                </svg>
-                <span className="sr-only">Close modal</span>
-              </button>
+                className="btn-close"
+                data-coreui-dismiss="modal"
+                aria-label="Close"
+              ></button>
             </div>
-            {/*content*/}
-            <div className="p-4 space-y-4 md:p-5">
+            <div className="modal-body">
               <ul className="flex flex-col items-center justify-start h-full gap-6">
                 <Link href="/" className="w-full text-white no-underline">
-                  <li className="flex items-center justify-center w-full text-black cursor-pointer text-nowrap max-lg:text-sm" data-modal-hide="nav-modal">
+                  <li
+                    className="flex items-center justify-center w-full text-black cursor-pointer text-nowrap max-lg:text-sm"
+                    data-coreui-dismiss="modal"
+                  >
                     Home
                   </li>
                 </Link>
                 <Link href="/menu" className="w-full text-white no-underline">
-                  <li className="flex items-center justify-center w-full text-black cursor-pointer text-nowrap max-lg:text-sm" data-modal-hide="nav-modal">
+                  <li
+                    className="flex items-center justify-center w-full text-black cursor-pointer text-nowrap max-lg:text-sm"
+                    data-coreui-dismiss="modal"
+                  >
                     Menu
                   </li>
                 </Link>
-                <Link href="/services" className="w-full text-white no-underline">
-                  <li className="flex items-center justify-center w-full text-black cursor-pointer text-nowrap max-lg:text-sm" data-modal-hide="nav-modal">
+                <Link
+                  href="/services"
+                  className="w-full text-white no-underline"
+                >
+                  <li
+                    className="flex items-center justify-center w-full text-black cursor-pointer text-nowrap max-lg:text-sm"
+                    data-coreui-dismiss="modal"
+                  >
                     Services
                   </li>
                 </Link>
-                <Link href="/aboutus" className="w-full text-white no-underline">
-                  <li className="flex items-center justify-center w-full text-black cursor-pointer text-nowrap max-lg:text-sm" data-modal-hide="nav-modal">
+                <Link
+                  href="/aboutus"
+                  className="w-full text-white no-underline"
+                >
+                  <li
+                    className="flex items-center justify-center w-full text-black cursor-pointer text-nowrap max-lg:text-sm"
+                    data-coreui-dismiss="modal"
+                  >
                     About Us
                   </li>
                 </Link>
-                <Link href="/contact" className="w-full text-white no-underline">
-                  <li className="flex items-center justify-center w-full text-black cursor-pointer text-nowrap max-lg:text-sm" data-modal-hide="nav-modal">
+                <Link
+                  href="/contact"
+                  className="w-full text-white no-underline"
+                >
+                  <li
+                    className="flex items-center justify-center w-full text-black cursor-pointer text-nowrap max-lg:text-sm"
+                    data-coreui-dismiss="modal"
+                  >
                     Contact Us
                   </li>
                 </Link>
-                <Link href="/projects" className="w-full text-white no-underline">
-                  <li className="flex items-center justify-center w-full text-black cursor-pointer text-nowrap max-lg:text-sm" data-modal-hide="nav-modal">
+                <Link
+                  href="/projects"
+                  className="w-full text-white no-underline"
+                >
+                  <li
+                    className="flex items-center justify-center w-full text-black cursor-pointer text-nowrap max-lg:text-sm"
+                    data-coreui-dismiss="modal"
+                  >
                     Projects
                   </li>
                 </Link>
                 <Link href="/csr" className="w-full text-white no-underline">
-                  <li className="flex items-center justify-center w-full text-black cursor-pointer text-nowrap max-lg:text-sm" data-modal-hide="nav-modal">
+                  <li
+                    className="flex items-center justify-center w-full text-black cursor-pointer text-nowrap max-lg:text-sm"
+                    data-coreui-dismiss="modal"
+                  >
                     CSR Projects
                   </li>
                 </Link>
