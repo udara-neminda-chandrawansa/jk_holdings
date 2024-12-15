a4fe220704c0c71eb0491ecc12de1cbbd2eebf30
Udara Neminda Chandrawansa <udaraneminda@gmail.com>
2024-12-15 21:49:16 +0530

    awards updated

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -45,6 +45,14 @@ import a_2 from "./assets/Awards/award-2.png";
 import a_3 from "./assets/Awards/award-3.png";
 import a_4 from "./assets/Awards/award-4.png";
 
+import img1 from "./assets/AboutGrid/img-1.png";
+import img2 from "./assets/AboutGrid/img-2.png";
+import img3 from "./assets/AboutGrid/img-3.png";
+import img4 from "./assets/AboutGrid/img-4.png";
+import img5 from "./assets/AboutGrid/img-5.png";
+import img6 from "./assets/AboutGrid/img-6.png";
+import img7 from "./assets/AboutGrid/img-7.png";
+
 //import add_1 from "./assets/adds/add-1.png";
 import add_2 from "./assets/adds/add-2.png";
 
@@ -705,24 +713,48 @@ function Landing() {
         </div>
         {/*awards grid*/}
         <div className="flex justify-center">
-        <div className="grid grid-cols-2 gap-4 max-md:grid-cols-1">
-          <div className="relative flex flex-col items-center">
-            <img src={a_1} alt="" className="z-10 object-contain w-full h-5/6"/>
-            <p className="w-full text-center h-1/6 bg-[#C10000] text-white flex items-center justify-center absolute bottom-10 max-md:bottom-8">Star Awards 2015</p>
+          <div className="grid grid-cols-3 gap-4 max-lg:grid-cols-2 max-md:grid-cols-1">
+            <div className="relative flex flex-col items-center">
+              <img
+                src={a_1}
+                alt=""
+                className="z-10 object-contain w-full h-5/6"
+              />
+              <p className="w-full text-center h-1/6 bg-[#C10000] text-white flex items-center justify-center absolute bottom-10 max-md:bottom-8">
+                Star Awards 2019
+              </p>
+            </div>
+            <div className="relative flex flex-col items-center">
+              <img
+                src={a_2}
+                alt=""
+                className="z-10 object-contain w-full h-5/6"
+              />
+              <p className="w-full text-center h-1/6 bg-[#C10000] text-white flex items-center justify-center absolute bottom-10 max-md:bottom-8">
+                Star Awards 2017
+              </p>
+            </div>
+            <div className="relative flex flex-col items-center">
+              <img
+                src={a_3}
+                alt=""
+                className="z-10 object-contain w-full h-5/6"
+              />
+              <p className="w-full text-center h-1/6 bg-[#C10000] text-white flex items-center justify-center absolute bottom-10 max-md:bottom-8">
+                Asian Heroes 2023
+              </p>
+            </div>
+            <div className="relative flex flex-col items-center">
+              <img
+                src={a_4}
+                alt=""
+                className="z-10 object-contain w-full h-5/6"
+              />
+              <p className="w-full text-center h-1/6 bg-[#C10000] text-white flex items-center justify-center absolute bottom-10 max-md:bottom-8">
+                Star Awards 2016
+              </p>
+            </div>
           </div>
-          <div className="relative flex flex-col items-center">
-            <img src={a_2} alt="" className="z-10 object-contain w-full h-5/6"/>
-            <p className="w-full text-center h-1/6 bg-[#C10000] text-white flex items-center justify-center absolute bottom-10 max-md:bottom-8">Star Awards 2016</p>
-          </div>
-          <div className="relative flex flex-col items-center">
-            <img src={a_3} alt="" className="z-10 object-contain w-full h-5/6"/>
-            <p className="w-full text-center h-1/6 bg-[#C10000] text-white flex items-center justify-center absolute bottom-10 max-md:bottom-8">Star Awards 2017</p>
-          </div>
-          <div className="relative flex flex-col items-center">
-            <img src={a_4} alt="" className="z-10 object-contain w-full h-5/6"/>
-            <p className="w-full text-center h-1/6 bg-[#C10000] text-white flex items-center justify-center absolute bottom-10 max-md:bottom-8">Star Awards 2018</p>
-          </div>
-        </div>
         </div>
         {/*Milestones*/}
         <div className="h-fit">
@@ -806,6 +838,213 @@ function Landing() {
           <span className="flex justify-end w-full px-12 py-3 underline max-md:px-6">
             <a href="https://www.google.com">View All</a>
           </span>
+        </div>
+        {/*Milestones - 2*/}
+        <div className="h-fit">
+          <div className="flex flex-col gap-6 p-12 h-fit max-md:p-6">
+            <span>
+              <h1 className="text-[30px] text-[#C10000] title">
+                Our Milestones
+              </h1>
+              <h1 className="text-[20px] sm:w-1/2 font-medium title text-center">
+                For a brighter future
+              </h1>
+            </span>
+            <p className="text-justify 2xl:text-2xl xl:text-xl max-md:text-lg max-sm:text-base">
+              At JKI Holdings, we believe in taking responsibility for our
+              actions, both as a company and as members of society. We
+              acknowledge the impact we have on the environment, the people we
+              work with, and the communities we operate in. Therefore, we have
+              initiated a range of projects and programs that aim to create a
+              positive impact on the world around us. Through our corporate
+              social responsibility (CSR) projects, we strive to promote
+              sustainability, equality, diversity, and inclusion. We are
+              committed to making a difference and creating a better future for
+              all.
+            </p>
+            {/*for md*/}
+            <div className="flex flex-col max-md:hidden md:h-[700px] gap-3 overflow-y-scroll red-custom-scroll">
+              {/*first row*/}
+              <div className="hidden h-[300px] gap-3 text-white md:flex">
+                <div className="flex flex-col flex-grow w-2/4 h-full">
+                  <div className="flex flex-col cursor-pointer">
+                    <img
+                      src={img1}
+                      alt="milestone-grid-card"
+                      className="h-[300px] object-cover"
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-col w-1/4 h-full cursor-pointer">
+                  <img
+                    src={img2}
+                    alt="milestone-grid-card"
+                    className="object-cover h-[300px]"
+                  />
+                </div>
+                <div className="flex flex-col w-1/4 h-full cursor-pointer">
+                  <img
+                    src={img3}
+                    alt="milestone-grid-card"
+                    className="object-cover h-[300px]"
+                  />
+                </div>
+              </div>
+              {/*second row*/}
+              <div className="hidden h-[400px] gap-3 text-white md:flex">
+                <div className="flex flex-col flex-grow w-3/5 h-full">
+                  <div className="flex flex-col h-full cursor-pointer">
+                    <img
+                      src={img4}
+                      alt=""
+                      className="object-cover object-top h-full"
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-col w-1/5 h-full gap-3 cursor-pointer">
+                  <img
+                    src={img5}
+                    alt="milestone-grid-card"
+                    className="object-cover h-1/2"
+                  />
+                  <img
+                    src={img6}
+                    alt="milestone-grid-card"
+                    className="object-cover h-1/2"
+                  />
+                </div>
+                <div className="flex flex-col w-1/5 h-full cursor-pointer">
+                  <img
+                    src={img7}
+                    alt="milestone-grid-card"
+                    className="object-cover h-[400px]"
+                  />
+                </div>
+              </div>
+              {/*third row*/}
+              <div className="hidden h-[300px] gap-3 text-white md:flex">
+                <div className="flex flex-col flex-grow w-2/4 h-full">
+                  <div className="flex flex-col cursor-pointer">
+                    <img
+                      src={img1}
+                      alt="milestone-grid-card"
+                      className="h-[300px] object-cover"
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-col w-1/4 h-full cursor-pointer">
+                  <img
+                    src={img2}
+                    alt="milestone-grid-card"
+                    className="object-cover h-[300px]"
+                  />
+                </div>
+                <div className="flex flex-col w-1/4 h-full cursor-pointer">
+                  <img
+                    src={img3}
+                    alt="milestone-grid-card"
+                    className="object-cover h-[300px]"
+                  />
+                </div>
+              </div>
+              {/*fourth row*/}
+              <div className="hidden h-[400px] gap-3 text-white md:flex">
+                <div className="flex flex-col flex-grow w-3/5 h-full">
+                  <div className="flex flex-col h-full cursor-pointer">
+                    <img
+                      src={img4}
+                      alt=""
+                      className="object-cover object-top h-full"
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-col w-1/5 h-full gap-3 cursor-pointer">
+                  <img
+                    src={img5}
+                    alt="milestone-grid-card"
+                    className="object-cover h-1/2"
+                  />
+                  <img
+                    src={img6}
+                    alt="milestone-grid-card"
+                    className="object-cover h-1/2"
+                  />
+                </div>
+                <div className="flex flex-col w-1/5 h-full cursor-pointer">
+                  <img
+                    src={img7}
+                    alt="milestone-grid-card"
+                    className="object-cover h-[400px]"
+                  />
+                </div>
+              </div>
+            </div>
+            {/*for max-md*/}
+            <div className="flex flex-col gap-3 text-white md:hidden">
+              <div className="bg-[#000000BD]">
+                <img
+                  src={img1}
+                  alt="milestone-grid-card"
+                  className="w-full max-h-[400px] object-contain"
+                />
+              </div>
+              <div className="bg-[#000000BD]">
+                <img
+                  src={img2}
+                  alt="milestone-grid-card"
+                  className="w-full max-h-[400px] object-contain"
+                />
+              </div>
+              <div className="bg-[#000000BD]">
+                <img
+                  src={img3}
+                  alt="milestone-grid-card"
+                  className="w-full max-h-[400px] object-contain"
+                />
+              </div>
+              <div className="bg-[#000000BD]">
+                <img
+                  src={img4}
+                  alt="milestone-grid-card"
+                  className="w-full max-h-[400px] object-contain"
+                />
+              </div>
+              <div className="bg-[#000000BD]">
+                <img
+                  src={img5}
+                  alt="milestone-grid-card"
+                  className="w-full max-h-[400px] object-contain"
+                />
+              </div>
+              <div className="bg-[#000000BD]">
+                <img
+                  src={img6}
+                  alt="milestone-grid-card"
+                  className="w-full max-h-[400px] object-contain"
+                />
+              </div>
+              <div className="bg-[#000000BD]">
+                <img
+                  src={img7}
+                  alt="milestone-grid-card"
+                  className="w-full max-h-[400px] object-contain"
+                />
+              </div>
+            </div>
+          </div>
+          {/*phrase*/}
+          <div className="flex justify-center pb-6 text-2xl poltawski">
+            <span className="inline text-[60px] max-md:text-2xl sansita max-lg:text-3xl max-xl:text-5xl text-[#C10000]">
+              “
+            </span>
+            <h1 className="w-1/2 text-center max-sm:w-fit max-sm:py-3">
+              Human Resources isn't a thing we do. It's the thing that runs our
+              business.
+            </h1>
+            <span className="inline text-[60px] max-md:text-2xl sansita max-lg:text-3xl max-xl:text-5xl text-[#C10000]">
+              ”
+            </span>
+          </div>
         </div>
         {/*Advertisements*/}
         <div className="p-12 max-md:p-6">
